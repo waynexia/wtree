3933ba85d0c17766d24c36713c22925dcf19f425
Ruihang Xia <waynestxia@gmail.com>
2019-09-11 19:43:27 +0800

    refine parameter parsing

--- src/envir.rs
+++ src/envir.rs
@@ -5,7 +5,7 @@ use std::process::exit;
 
 #[macro_use]
 lazy_static! {
-    static ref SETTING: Setting = parase_parameter();
+    static ref SETTING: Setting = parse_parameter();
 }
 
 pub struct Setting {
@@ -36,117 +36,35 @@ pub struct Setting {
     pub root: PathBuf,
 }
 
-fn parase_parameter() -> Setting {
-    let mut args: Vec<String> = env::args().collect();
-    // remove the first arg which is command name
-    args.remove(0);
-    //extract last parameter, if not a path, put it back
-    let root_path: PathBuf = if let Some(path) = args.pop() {
-        if PathBuf::from(path.clone()).is_dir() {
-            PathBuf::from(path.clone())
-        } else {
-            args.push(path);
-            PathBuf::from("./".to_string())
+impl Default for Setting {
+    fn default() -> Setting {
+        Setting {
+            is_all: false,
+            is_dir_only: false,
+            is_no_indentation: false,
+            is_sort_alphanumerically: true,
+            is_sort_mod_time: false,
+            is_unsort: false,
+            is_sort_reverse: false,
+            is_dir_first: false,
+            is_needing_report: true,
+            is_quote: false,
+            is_color: false,
+            is_full_path: false,
+            pattern_i: false,
+            pattern_p: false,
+            pattern_ignore_case: false,
+            need_protection: false,
+            need_uid: false,
+            need_gid: false,
+            need_size: 0,
+            need_ctime: false,
+            need_inode: false,
+            need_device: false,
+            pattern: String::new(),
+            root: PathBuf::new(),
         }
-    } else {
-        PathBuf::from("./".to_string())
     }
-    .canonicalize()
-    .unwrap();
-
-    //println!("{:?}",root_path);
-    //panic!();
-
-    let mut ret = Setting {
-        is_all: false,
-        is_dir_only: false,
-        is_no_indentation: false,
-        is_sort_alphanumerically: true,
-        is_sort_mod_time: false,
-        is_unsort: false,
-        is_sort_reverse: false,
-        is_dir_first: false,
-        is_needing_report: true,
-        is_quote: false,
-        is_color: false,
-        is_full_path: false,
-        pattern_i: false,
-        pattern_p: false,
-        pattern_ignore_case: false,
-        need_protection: false,
-        need_uid: false,
-        need_gid: false,
-        need_size: 0,
-        need_ctime: false,
-        need_inode: false,
-        need_device: false,
-        pattern: "".to_string(),
-
-        root: root_path.canonicalize().unwrap(),
-    };
-
-    let mut args_iter = args.iter().peekable();
-    while args_iter.peek() != Option::None {
-        let i: &String = args_iter.next().unwrap();
-        match i.as_ref() {
-            "-a" => ret.is_all = true,
-            "-d" => ret.is_dir_only = true,
-            "-i" => ret.is_no_indentation = true,
-            "-v" => ret.is_sort_alphanumerically = true,
-            "-t" => ret.is_sort_mod_time = true,
-            "-U" => ret.is_unsort = true,
-            "-r" => ret.is_sort_reverse = true,
-            "--dirsfirst" => ret.is_dir_first = true,
-            "--noreport" => ret.is_needing_report = false,
-            "--ignore-case" => ret.pattern_ignore_case = true,
-            "-Q" => ret.is_quote = true,
-            "-f" => ret.is_full_path = true,
-            // -n will be overwrite, no reaction
-            "-C" => ret.is_color = true,
-            "-p" => ret.need_protection = true,
-            "-u" => ret.need_uid = true,
-            "-g" => ret.need_gid = true,
-            "-D" => ret.need_ctime = true,
-            "--inode" => ret.need_inode = true,
-            "--device" => ret.need_device = true,
-            // -s, -h, --si will override others
-            "-s" => ret.need_size = 1,
-            "-h" => ret.need_size = 2,
-            "--si" => ret.need_size = 3,
-            "-P" => {
-                ret.pattern_p = true;
-                // only can exist one pattern
-                if ret.pattern_p && ret.pattern_i {
-                    ret.pattern_i = false;
-                }
-                let pattern: &str = args_iter.peek().expect("need a pattern here");
-                ret.pattern = pattern.to_string();
-            }
-            "-I" => {
-                ret.pattern_i = true;
-                // only can exist one pattern
-                if ret.pattern_p && ret.pattern_i {
-                    ret.pattern_p = false;
-                }
-                let pattern: &str = args_iter.peek().expect("need a pattern here");
-                ret.pattern = pattern.to_string();
-            }
-
-            "--help" => {
-                Setting::print_help();
-                exit(0);
-            }
-            "--version" => {
-                println!("wtree, version 0.2.0 by @waynexia");
-                exit(0);
-            }
-
-            _ => Setting::error_report("Invalid argument: ".to_string() + i.as_ref()),
-        }
-        args_iter.next();
-    }
-
-    ret
 }
 
 impl Setting {
@@ -189,8 +107,8 @@ usage: tree [-acdfghilnpqrstuvxACDFJQNSUX] [-H baseHREF] [-T title ]
   -Q            Quote filenames with double quotes.
   -p            Print the protections for each file.
   -u            Displays file owner or UID number.
-  -g            Displays file group owner or GID number.
-  -s            Print the size in bytes of each file.
+  -g            Displays file group owner or GID number.content
+  -s            Print the size in bytes of each file.content
   -h            Print the size in a more human readable way.
   --si          Like -h, but use in SI units (powers of 1000).
   -D            Print the date of last modification or (-c) status change.
@@ -225,6 +143,160 @@ usage: tree [-acdfghilnpqrstuvxACDFJQNSUX] [-H baseHREF] [-T title ]
   --            Options processing terminator.
         "}
     }
+}
+
+fn need_extra_para(flag: &String) -> bool {
+    flag.eq("H")
+        || flag.eq("T")
+        || flag.eq("L")
+        || flag.eq("P")
+        || flag.eq("I")
+        || flag.eq("o")
+        || flag.eq("charset")
+        || flag.eq("filelimit")
+        || flag.eq("timefmt")
+        || flag.eq("sort")
+}
+
+/*
+    @brief
+        insert into decomposed args,
+        check for flags that need extra parameter
+*/
+fn vec_push_hook(
+    vec: &mut Vec<String>,
+    item: &String,
+    expect_pattern: &mut bool,
+) -> Result<(), ()> {
+    if *expect_pattern == true {
+        return Err(());
+    }
+
+    if need_extra_para(item) {
+        *expect_pattern = true;
+    }
+
+    vec.push(item.clone());
+    Ok(())
+}
+
+fn decompose_arg(args: &Vec<String>) -> Result<Vec<String>, ()> {
+    let mut expect_pattern = false;
+    let mut ret_args: Vec<String> = Vec::new();
+
+    for arg in args {
+        if arg.starts_with("--") {
+            if let Some(substr) = arg.get(2..) {
+                vec_push_hook(&mut ret_args, &substr.to_string(), &mut expect_pattern)?
+            };
+        } else if arg.starts_with("-") {
+            if let Some(substr) = arg.get(1..) {
+                for abbr in substr.chars() {
+                    vec_push_hook(&mut ret_args, &abbr.to_string(), &mut expect_pattern)?;
+                }
+            }
+        } else if expect_pattern {
+            ret_args.push(arg.to_string());
+            expect_pattern = false;
+        } else {
+            return Err(());
+        }
+    }
+    Ok(ret_args)
+}
+
+fn parse_parameter() -> Setting {
+    let mut args: Vec<String> = env::args().collect();
+    // remove the first arg which is command name
+    args.remove(0);
+    //extract last parameter, if not a path, put it back
+    let root_path: PathBuf = if let Some(path) = args.pop() {
+        if PathBuf::from(path.clone()).is_dir() {
+            PathBuf::from(path.clone())
+        } else {
+            args.push(path);
+            PathBuf::from("./".to_string())
+        }
+    } else {
+        PathBuf::from("./".to_string())
+    }
+    .canonicalize()
+    .unwrap();
+    let mut ret = Setting {
+        root: root_path,
+        ..Default::default()
+    };
+
+    let wtf = match decompose_arg(&args) {
+        Ok(vec) => vec,
+        Err(_) => {
+            Setting::print_help();
+            exit(0);
+        }
+    };
+    let mut args_iter = wtf.iter().peekable();
+
+    while args_iter.peek() != Option::None {
+        let i: &String = args_iter.next().unwrap();
+        match i.as_ref() {
+            "a" => ret.is_all = true,
+            "d" => ret.is_dir_only = true,
+            "i" => ret.is_no_indentation = true,
+            "v" => ret.is_sort_alphanumerically = true,
+            "t" => ret.is_sort_mod_time = true,
+            "U" => ret.is_unsort = true,
+            "r" => ret.is_sort_reverse = true,
+            "dirsfirst" => ret.is_dir_first = true,
+            "noreport" => ret.is_needing_report = false,
+            "ignore-case" => ret.pattern_ignore_case = true,
+            "Q" => ret.is_quote = true,
+            "f" => ret.is_full_path = true,
+            // -n will be overwrite, no reaction
+            "C" => ret.is_color = true,
+            "p" => ret.need_protection = true,
+            "u" => ret.need_uid = true,
+            "g" => ret.need_gid = true,
+            "D" => ret.need_ctime = true,
+            "inodes" => ret.need_inode = true,
+            "device" => ret.need_device = true,
+            // -s, -h, --si will override others
+            "s" => ret.need_size = 1,
+            "h" => ret.need_size = 2,
+            "si" => ret.need_size = 3,
+            "P" => {
+                ret.pattern_p = true;
+                // only can exist one pattern
+                if ret.pattern_p && ret.pattern_i {
+                    ret.pattern_i = false;
+                }
+                let pattern: &str = args_iter.peek().expect("need a pattern here");
+                ret.pattern = pattern.to_string();
+            }
+            "I" => {
+                ret.pattern_i = true;
+                // only can exist one pattern
+                if ret.pattern_p && ret.pattern_i {
+                    ret.pattern_p = false;
+                }
+                let pattern: &str = args_iter.peek().expect("need a pattern here");
+                ret.pattern = pattern.to_string();
+            }
+
+            "help" => {
+                Setting::print_help();
+                exit(0);
+            }
+            "version" => {
+                println!("wtree, version 0.2.0 by @waynexia");
+                exit(0);
+            }
+
+            _ => Setting::error_report("Invalid argument: ".to_string() + i.as_ref()),
+        }
+        args_iter.next();
+    }
+
+    ret
 }
 
 impl Setting {
@@ -328,4 +400,24 @@ impl Setting {
     pub fn need_device() -> bool {
         SETTING.need_device
     }
+}
+
+#[test]
+fn test_fn_decompose_arg() {
+    let args = decompose_arg(&vec![String::from("-vga"), String::from("--inodes")]).unwrap();
+    assert_eq!(
+        args,
+        vec![
+            String::from("v"),
+            String::from("g"),
+            String::from("a"),
+            String::from("inodes")
+        ]
+    )
+}
+
+#[test]
+#[should_panic]
+fn decompose_arg_with_incorrect_input() {
+    let _args = decompose_arg(&vec![String::from("-Io")]).unwrap();
 }
